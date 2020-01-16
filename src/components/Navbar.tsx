72a29ce09bf2e01ef1ad9f3723f5ecff95f5ed07
doublev32 <valentin.van32@gmail.com>
2020-01-16 23:46:15 +0800

    Cleaning project + update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
-require('./styles/navbar.scss');
+import logo from '../assets/img/logo.png';
+require('../styles/components/navbar.scss');
 
 interface Props {
     currentPage: string
@@ -11,47 +12,20 @@ export default class Navbar extends React.Component<Props, object> {
         <section className="navigation">
         <div className="nav-container">
             <div className="brand">
-            <a href="#!">Logo</a>
+            <a href="#!"><img src={logo}/></a>
             </div>
             <nav>
             <div className="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
             <ul className="nav-list">
                 <li>
                 <a href="#!">Home</a>
+                <div className="item-line"></div>
+                </li>
+                <li>
+                <a href="#!">Menu 1</a>
                 </li>
                 <li>
                 <a href="#!">About</a>
-                </li>
-                <li>
-                <a href="#!">Services</a>
-                <ul className="nav-dropdown">
-                    <li>
-                    <a href="#!">Web Design</a>
-                    </li>
-                    <li>
-                    <a href="#!">Web Development</a>
-                    </li>
-                    <li>
-                    <a href="#!">Graphic Design</a>
-                    </li>
-                </ul>
-                </li>
-                <li>
-                <a href="#!">Pricing</a>
-                </li>
-                <li>
-                <a href="#!">Portfolio</a>
-                <ul className="nav-dropdown">
-                    <li>
-                    <a href="#!">Web Design</a>
-                    </li>
-                    <li>
-                    <a href="#!">Web Development</a>
-                    </li>
-                    <li>
-                    <a href="#!">Graphic Design</a>
-                    </li>
-                </ul>
                 </li>
                 <li>
                 <a href="#!">Contact</a>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import React, { ReactNode } from "react";
 import Navbar from "../components/Navbar";
+require('../styles/home.scss');
 
 export default class Home extends React.Component<object, object> {
     render() {
         return (
-        <div>
-            <Navbar 
-                currentPage={"home"}
-            />
-        </div>
+            <div className="home">
+                <Navbar 
+                    currentPage={"home"}
+                />
+            </div>
         );
     }
 
